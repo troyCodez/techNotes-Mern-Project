c014c9d5c194f641e3819a02126f4fb2cc31aaef
troydelageto <tambongroy7@gmail.com>
2025-04-24 16:49:48 +0100

    file structure with routes,views folder

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT =process.env.PORT || 3000;
+const PORT =process.env.PORT || 3500;
 
 
 // Css file rendering
 app.use('/', express.static(path.join(__dirname, '/public')));
 
+// Router rendering
+app.use('/', require('./routes/root'))
 
 // Port listening
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
