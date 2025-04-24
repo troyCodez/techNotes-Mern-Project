c19b2a166b7c6303112b4187d8a36ff2548c89fd
troydelageto <tambongroy7@gmail.com>
2025-04-24 17:11:55 +0100

    updated logs and middleware folder

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,22 @@ const PORT =process.env.PORT || 3500;
 
 
 // Css file rendering
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Router rendering
 app.use('/', require('./routes/root'))
+
+app.call('*',(req,res)=>{
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'))
+    } else if(req.accepts('json')){
+        res.json({message:'404 Not Found'})
+        
+    }else {
+        res.type('txt').send('404 Not found')
+    }
+})
 
 // Port listening
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
